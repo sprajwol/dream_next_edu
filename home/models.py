683891150515db6c38305f18481a3282a8886487
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-09-22 22:37:28 +0545

    added slider model;migrated;jquery added;

--- home/models.py
+++ home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+def get_hero_slider_img_upload_path(instance, filename):
+    # filename, ext = filename.split('.')
+    return f'uploads/heroSlider/{filename}'
+
+
+SLIDER_CONTENT_POSITION = (
+    ('left', 'left'),
+    ('right', 'right'),
+    ('center', 'center'),
+)
+
+
+class HeroSlider(models.Model):
+    main_text = models.CharField(max_length=255)
+    sub_text = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to=get_hero_slider_img_upload_path, blank=True, null=True)
+    content_position = models.CharField(
+        choices=SLIDER_CONTENT_POSITION, max_length=10)
+
+    def __str__(self):
+        return str(self.main_text)
